27ec6aca347c8a1bd43d52e9bf5ea0ad4fa1e345
songhui339 <csonghui339@gmail.com>
2022-12-16 20:56:11 +0900

    👩🏻‍🔧 변수 선언 키워드 변경

--- JS/mainPage.js
+++ JS/mainPage.js
@@ -10,9 +10,9 @@ jQuery(document).ready(function ($) {
     타이핑 효과
     ===================
     */
-    var content = "안녕하세요 :-) \n 개발자를 꿈꾸는 최송희입니다. \n";
-    var text = document.querySelector(".text");
-    var i = 0;
+    let content = "안녕하세요 :-) \n 개발자를 꿈꾸는 최송희입니다. \n";
+    let text = document.querySelector(".text");
+    let i = 0;
 
     function typing(){
         let txt = content[i++];
@@ -29,14 +29,14 @@ jQuery(document).ready(function ($) {
     이미지 슬라이드
     ===================
     */
-    var imgIndex = 0;
-    var position = 0;
-    var IMG_WIDTH = 450;
-    var $btnPrev = document.querySelector(".btn-prev");
-    var $btnNext = document.querySelector(".btn-next");
-    var $slideImgs = document.querySelector(".imgList");
+    let imgIndex = 0;
+    let position = 0;
+    let IMG_WIDTH = 450;
+    let $btnPrev = document.querySelector(".btn-prev");
+    let $btnNext = document.querySelector(".btn-next");
+    let $slideImgs = document.querySelector(".imgList");
 
-    var prev = function () {
+    let prev = function () {
     if (imgIndex > 0) {
         $btnNext.removeAttribute("disabled");
         position += IMG_WIDTH;
@@ -48,7 +48,7 @@ jQuery(document).ready(function ($) {
     }
     };
 
-    var next = function () {
+    let next = function () {
     if (imgIndex < 3) {
         $btnPrev.removeAttribute("disabled");
         position -= IMG_WIDTH;
@@ -61,7 +61,7 @@ jQuery(document).ready(function ($) {
     }
     };
 
-    var init = function () {
+    let init = function () {
     $btnPrev.setAttribute("disabled", "true");
     $btnPrev.addEventListener("click", prev);
     $btnNext.addEventListener("click", next);
@@ -78,8 +78,8 @@ Skills 탭
 ===================
 */
 jQuery(document).ready(function ($) {
-    var btnAll = $('.skillTabRow_01 .btn_container section .btnBox');
-    var tabAll = $('.skillTabRow_02 .detail_container');
+    let btnAll = $('.skillTabRow_01 .btn_container section .btnBox');
+    let tabAll = $('.skillTabRow_02 .detail_container');
     
     
     // 프론트엔드
@@ -142,7 +142,7 @@ jQuery(document).ready(function ($) {
 	$(".accordion > li:eq(0) a").addClass("active").next().slideDown();
 
 	$(".accordion a").click(function (e) {
-		var dropDown = $(this).closest("li").find(".textBox");
+		let dropDown = $(this).closest("li").find(".textBox");
 		$(this).closest(".accordion").find(".textBox").not(dropDown).slideUp();
 		if ($(this).hasClass("active")) {
 			$(this).removeClass("active");
@@ -162,11 +162,11 @@ Flip Card
 ===================
 */
 jQuery(document).ready(function ($) {
-    var btn = document.querySelector('.flipBtn');
+    let btn = document.querySelector('.flipBtn');
     btn.addEventListener('click', click);
     
     function click(event) {
-        var card = document.querySelector('.flipCard');
+        let card = document.querySelector('.flipCard');
         if (card.style.transform == "rotateY(180deg)") {
             card.style.transform = "rotateY(0deg)";
                 } else {
